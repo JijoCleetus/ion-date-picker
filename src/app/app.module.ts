fe5c6c8d0049fa2806ee2e526f7566d98d047ab8
JijoCleetus <jijocleetus@gmail.com>
2024-12-09 02:12:31 +0000

    feat: code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { IonicAppModule } from './ionic.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, FormsModule, IonicModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/ionic.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonInput, IonLabel } from '@ionic/angular';
+
+@NgModule({
+  declarations: [],
+  imports: [BrowserModule],
+})
+export class IonicAppModule {}
